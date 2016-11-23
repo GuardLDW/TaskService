07989c9696f5be013f4fb05dee935b4cea8e456c
GuardLDW <303899225@qq.com>
2016-11-23 13:51:25 +0800

    Finish Homework 8

--- src/TaskCondition.ts
+++ src/TaskCondition.ts
@@ -9,7 +9,6 @@ interface TaskCondition{
 
 class NPCTalkTaskCondition implements TaskCondition{
 
-    
     //Task与TaskConditionContext的区别
     //不想让NPCTalkTaskCondition得到task的所有信息
     onAccept(context : TaskConditionContext){
